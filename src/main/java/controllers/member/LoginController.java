629dda361a28d322f5371e3ccc8cb39337643abb
daeyounggg <kdyzab@gmail.com>
2023-10-23 15:35:48 +0900

    기능통합 - 회원가입

--- src/main/java/controllers/member/LoginController.java
+++ src/main/java/controllers/member/LoginController.java
@@ -6,8 +6,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import models.member.LoginService;
+import models.member.ServiceManager;
 
 import java.io.IOException;
+
+import static commons.ScriptUtil.alertError;
+import static commons.ScriptUtil.go;
 
 @WebServlet("/member/login")
 public class LoginController extends HttpServlet {
@@ -19,6 +24,14 @@ public class LoginController extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       try {
+           LoginService service = ServiceManager.getInstance().loginService();
+           service.login(req);
 
+           go(resp, req.getContextPath() + "/", "parent");
+
+       }catch (RuntimeException e){
+           alertError(resp, e);
+       }
     }
 }
